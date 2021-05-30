7f175af91eb96419a44faa1c9f15fff4ad884525
Diep Quynh <remilia.1505@gmail.com>
2021-05-31 06:49:23 +0700

    AK3: Initial Exynos 8895 adaption
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,23 +4,21 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=RZ Kernel for Exynos 8895 devices
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
+device.name1=dreamlte
+device.name2=dream2lte
+device.name3=greatlte
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/platform/11120000.ufs/by-name/BOOT;
 is_slot_device=0;
 ramdisk_compression=auto;
 
@@ -33,31 +31,48 @@ ramdisk_compression=auto;
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
+set_perm_recursive 0 0 750 750 $ramdisk/init*;
+set_perm_recursive 0 0 755 755 $ramdisk/rz;
+set_perm 0 0 750 $ramdisk/*.rc;
 
 ## AnyKernel install
+system_path=/system
+# SAR check
+if [ -e /system_root ]; then
+	ui_print "System-as-root detected!...";
+	mount -o remount,rw /system_root;
+	system_path=/system_root/system;
+	rm -f /system_root/system/vendor/etc/init/init.services.rc;
+	insert_line /system_root/init.rc "init.services.rc" after "import /init.environ.rc" "import /init.services.rc\n";
+	mv -f $ramdisk/init.services.rc /system_root;
+	mv -f $ramdisk/fstab.samsungexynos8895 /system_root/fstab.samsungexynos8895;
+	rm -rf /system_root/rz;
+	mv -f $ramdisk/rz /system_root/rz;
+else
+	mount -o remount,rw /system;
+fi;
+
+# Unpack boot image
 dump_boot;
 
-# begin ramdisk changes
+# Cleanup
+ui_print "Cleaning old RZ leftovers...";
+rm -f $ramdisk/rz/scripts/40perf;
+rm -f $ramdisk/rz/scripts/90userinit;
+rm -f $system_path/bin/sysinit_cm;
+rm -f $system_path/etc/init.d/30zram;
+rm -f $system_path/etc/init.d/40perf;
+rm -f $system_path/etc/init.d/90userinit;
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+if [ ! -e /system_root ]; then
+	insert_line $ramdisk/init.rc "init.services.rc" after "import /init.environ.rc" "import /init.services.rc\n";
+	remove_line $ramdisk/fstab.samsungexynos8895 /dev/block/platform/11120000.ufs/by-name/CPEFS;
+fi;
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
-
-# end ramdisk changes
+# Check device dtb
+device_name=$(file_getprop /default.prop ro.product.device);
+mv -f $home/*${device_name}*/Image $home/Image;
+mv -f $home/*${device_name}*/dtb_$device_name.img $split_img/extra;
 
 write_boot;
 ## end install
